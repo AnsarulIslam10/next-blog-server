c3ed54bd972c7cdc90c8315097a9a11144c013cb
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-09-16 10:16:30 +0600

    getUserById, updateUser, deleteUser

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,8 +1,27 @@
-import  express from 'express';
+import express from 'express';
 import { UserController } from './user.controller';
 const router = express.Router();
 
-router.get("/", UserController.getAllFromDB)
-router.post("/", UserController.createUser)
+router.get(
+    "/", 
+    UserController.getAllFromDB
+)
+router.get(
+    "/:id",
+    UserController.getUserById
+)
+router.post(
+    "/", 
+    UserController.createUser
+)
+router.patch(
+    "/:id",
+    UserController.updateUser
+)
+
+router.delete(
+    "/:id",
+    UserController.deleteUser
+)
 
 export const userRouter = router;
